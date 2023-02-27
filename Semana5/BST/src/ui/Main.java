46ebd88ae358f465e9fac7e9b8b0cffbe36fdfcc
njse22 <njse22@gmail.com>
2023-02-27 08:54:47 -0500

    delete tree

---
+++ Semana5/BST/src/ui/Main.java
@@ -0,0 +1,40 @@
+package ui;
+
+import model.*;
+
+public class Main{
+
+	public static void main(String[] args) {
+		BST tree = new BST(); 
+
+		tree.insert(new Node(50)); 
+		tree.insert(new Node(40)); 
+		tree.insert(new Node(60)); 
+		tree.insert(new Node(45)); 
+		tree.insert(new Node(55)); 
+		tree.insert(new Node(30)); 
+		tree.insert(new Node(70)); 
+
+		tree.inOrder(); 
+		System.out.println("");
+		System.out.print(tree.inOrderString());
+
+		tree.delete(30);
+
+		System.out.println("");
+		System.out.print(tree.inOrderString());
+
+
+		tree.delete(60);
+
+		System.out.println("");
+		System.out.print(tree.inOrderString());
+
+		tree.delete(50);
+
+		System.out.println("");
+		System.out.print(tree.inOrderString());
+
+		
+	}
+}
